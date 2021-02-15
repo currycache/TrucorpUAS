bd92e0a537371ef10ccd8f32e5b58d97d7968f93
currychace <verennetanara@gmail.com>
2021-02-15 10:35:46 -0500

    file php

--- website/veren.php
+++ website/veren.php
@@ -3,10 +3,8 @@ $connect = new musqli("172.19.0.2", "root", "superpassword", "Trucorp");
 
 $sql = "SELECT * FROM users";
 $result = $connect->query($sql);
-
 echo "total data adalah = ";
 echo ($result->num_rows);
-
 $connect->close();
 
 ?>
